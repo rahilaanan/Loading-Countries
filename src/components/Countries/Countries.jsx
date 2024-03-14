c824662b7c756ca27f3c9680dec96c3a5c54132f
rahilaanan.18@gmail.com <rahilaanan.18@gmail.com>
2024-03-14 19:47:22 +0800

    dynamically added country lists

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
-import './Countries.css'
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries] =useState([]);
+  const [countries, setCountries] = useState([]);
+  const [add, setAdd] = useState([]);
 
-    useEffect(()=>{
-        fetch("https://restcountries.com/v3.1/all")
-        .then(res=>res.json())
-        .then(data=>setCountries(data))
-    },[])
-    return (
-        <div>
-            <p>Country: {countries.length}</p>
-            <div className="Countries">
-            {
-                countries.map(countri=><Country key={countri.cca3} country={countri}></Country>)
-            }
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  const handleAdd = (country) => {
+    console.log("added");
+    const newAdd = [...add, country];
+    setAdd(newAdd);
+  };
+  return (
+    <div>
+      <p>Country: {countries.length}</p>
+      <div>
+        <h4>Visited Countries {add.length}</h4>
+        <ul>
+          {add.map((country) => (
+            <li  key={country.cca3}> {country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="Countries">
+        {countries.map((countri) => (
+          <Country
+            key={countri.cca3}
+            handleAdd={handleAdd}
+            country={countri}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Countries; 
+export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
-  const {name,flags,region,cca3}=country
+const Country = ({country,handleAdd}) => {
+  const {name,flags,region,cca3}=country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisit = ()=>{
+    setVisited(!visited);
+    
+  }
+  const passWithParams = ()=>handleAdd(country);
     return (
         <div className='country'>
             <h3>Name:{name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Region: {region}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParams}>Add to list</button>
+            <br />
+            <br />
+            <button onClick={handleVisit}>Visited</button>
+            {visited ? "  visited": "  not yet"}
         </div>
     );
 };
